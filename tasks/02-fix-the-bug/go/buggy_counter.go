065b7fad7d8bc65ecea05dd62f23ef64cc7bbf5e
TS <thanyawat.s@7solutions.co.th>
2025-06-18 13:30:46 +0700

    refactor NextID function to eliminate redundant increment of current

--- tasks/02-fix-the-bug/go/buggy_counter.go
+++ tasks/02-fix-the-bug/go/buggy_counter.go
@@ -8,8 +8,8 @@ import (
 var current int64
 
 func NextID() int64 {
+	// use atomic to add a current to avoid a race
 	id := atomic.AddInt64(&current, 1) - 1
 	time.Sleep(0)
-	current++
 	return id
 }
