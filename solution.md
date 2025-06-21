413ebd115f530e02c8418044e64b746cc6d6b8ea
PAKAPON CHAIMONGKOLTRAKUL <pakapon.chaimongkoltrakul@siemens.com>
2025-06-21 13:08:05 +0700

    1st task complete

---
+++ solution.md
@@ -0,0 +1,8 @@
+## Solution notes
+
+### Task 01 – Run‑Length Encoder
+- Language: python
+- Approach: I create a count dict first to store each char and its count then concate it later
+- Why: I have an experience using this to quickly query the data.
+- Time spent: 5 min
+- AI tools used: None
--- tasks/01-run-length/python/rle.py
+++ tasks/01-run-length/python/rle.py
@@ -5,4 +5,21 @@ def encode(s: str) -> str:
     >>> encode("AAB") -> "A2B1"
     """
     # TODO: implement
-    raise NotImplementedError("Implement me!")
+    # raise NotImplementedError("Implement me!")
+
+    count_dict: dict[str, int] = {}
+    for character in s:
+        count_dict[character] = count_dict.setdefault(character, 0) + 1
+    concate_str: str = ''
+    for character, count_value in count_dict.items():
+        concate_str = concate_str + character + str(count_value) 
+    return concate_str
+        
+
+if __name__ == "__main__":
+    # encode("")                              → ""
+    # encode("XYZ")                           → "X1Y1Z1"
+    # encode("AAAaaaBBB🦄🦄🦄🦄🦄CCCCCCCCCCCC") → "A3a3B3🦄5C12"
+    # encode("HAAAAPPY🦄")                    → "H1A4P2Y1🦄1"
+    
+    print(encode("AAAaaaBBB🦄🦄🦄🦄🦄CCCCCCCCCCCC"))
