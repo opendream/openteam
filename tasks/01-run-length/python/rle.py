2dab5bff28bf45a0bf304367886f6175a6fc2fd8
Siriram Hazam <mickykubpom@outlook.co.th>
2025-06-22 21:58:11 +0700

    Implement Task 01

--- tasks/01-run-length/python/rle.py
+++ tasks/01-run-length/python/rle.py
@@ -5,4 +5,23 @@ def encode(s: str) -> str:
     >>> encode("AAB") -> "A2B1"
     """
     # TODO: implement
-    raise NotImplementedError("Implement me!")
+    # raise NotImplementedError("Implement me!")
+    if not s:
+        return ""
+    
+    result = []
+    current_char = s[0]
+    count = 1
+
+    for char in s[1:]:
+        if char == current_char:
+            count += 1
+        else:
+            result.append(f"{current_char}{count}")
+            current_char = char
+            count = 1
+
+    result.append(f"{current_char}{count}")
+    return "".join(result)
+
+print(encode("AAAaaaBBB🦄🦄🦄🦄🦄CCCCCCCCCCCC"))
